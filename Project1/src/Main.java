dbe299ebb59bcea21a7192c2b2d7262449a30aaa
JurassicSPb <Jurassic5@yandex.ru>
2016-05-14 18:26:50 +0300

    added new files Project1 q

---
+++ Project1/src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Юрий on 14.05.2016.
+ */
+public class Main {
+    public static void main (String [] args) {
+
+    System.out.println (" (\\_/)" + "\n" + "(=‘ . ’=)" + "\n" + " (“)_(“)");
+    // делаем экранирование символа \ поставив перед ним еще один \
+    // экарнирование " тоже ставим перед ним \
+
+    System.out.println (" ");
+    System.out.println (" ");
+        int a = 5;
+        int b = 0;
+        double x = -b/a;
+        System.out.println (x);
+
+
+
+
+
+
+
+
+}
+}
